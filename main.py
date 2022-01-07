60a8efc2e5ff424c67a71618171711e9d1b58972
matheusAparicio <matheusilva-m@outlook.com>
2022-01-07 16:21:34 -0300

    novas formas de introduzir emails

--- main.py
+++ main.py
@@ -2,17 +2,67 @@ from web_scraper import start_scrape
 from urllib.request import urlopen, Request
 
 def main():
+
+    command = 0
+    certain = 'N'
+    login = ""
+    password = ""
+    subject = ""
+    content = ""
+    emailCurrent = ""
+    emails = []
+    webpage = ""
+
+    print("Bem vinda(o) ao nosso sistema! Digite o número da opção desejada e tecle enter.")
+
+    while(command != 1 and command != 2 and command != 3 or (certain != 'S' and certain != 's')):
+        command = int(input("""
+    1: Você digita manualmente os e-mails para o qual quer enviar mensagens.
+    2: Você digita um site onde será feito webscrapping para obter os e-mails do qual serão enviadas as mensagens.
+    3: Você adiciona e-mails manualmente e indica um site para mais e-mails a serem obtidos.
+    Comando: """))
+
+        if (command == 1):
+            certain = input("Você escolheu a opção 1: Adicionar e-mails manualmente.\nTem certeza? (S/N): ")
+        elif (command == 2):
+            certain = input("Você escolheu a opção 2: Adicionar e-mails automaticamente.\nTem certeza? (S/N): ")
+        elif (command == 3):
+            certain = input("Você escolheu a opção 3: Adicionar e-mails manualmente e automaticamente.\nTem certeza? (S/N): ")
+        else:
+            print("Comando não reconhecido.")
     
-    webpage = input("Cole o link para fazer webscraping de emails: ")
+    login = input("Digite o e-mail remetente: ")
+    password = input("Digite a senha do e-mail remetente: ")
+    subject = input("Digite o assunto dos e-mails: ")
+    content = input("Digite o conteúdo dos e-mails:\n")
+
+    if (command == 1 or command == 3):
+        emailCurrent = input("Digite o e-mail 1: ")
+        emails.append(emailCurrent)
+        while (emailCurrent != ""):
+            emailCurrent = input(f"Digite o e-mail {len(emails)+1}: ")
+            if emailCurrent != "":
+                emails.append(emailCurrent)
+
+    if (command == 2 or command == 3):
+        webpage = input("Cole o link para fazer webscraping de emails: ")
 
     try:
-        page = urlopen(webpage) 
-        start_scrape(page)
+        if command == 1:
+            start_scrape(subject, content, login, password, emailsManual=emails)
+        elif command == 2:
+            page = urlopen(webpage)
+            start_scrape(subject, content, login, password, page=page)
+        else:
+            page = urlopen(webpage)
+            start_scrape(subject, content, login, password, emailsManual=emails, page=page)
     except:
-        hdr = {'User-Agent': 'Mozilla/5.0'}
-        req = Request(webpage, headers=hdr)
-        page = urlopen(req)
-        start_scrape(page)
+        if command != 1:
+            hdr = {'User-Agent': 'Mozilla/5.0'}
+            req = Request(webpage, headers=hdr)
+            page = urlopen(req)
+            start_scrape(subject, content, login, password, emailsManual=emails, page=page)
+        print("caiu no except")
 
 if __name__ == "__main__":
     main()
--- web_scraper.py
+++ web_scraper.py
@@ -5,20 +5,21 @@ from openpyxl import Workbook
 from bs4 import BeautifulSoup
 from segredo import EMAIL_ADRESS, EMAIL_PASSWORD
 
-
 enviar_email = True
 
 book = Workbook()
 sheet = book.active
 
 
-def start_scrape(subject:str, content:str, sender:str, page=''):
+def start_scrape(subject:str, content:str, login:str, password='', emailsManual=[], page=''):
     
     scrape = BeautifulSoup(page, 'html.parser')
     scrape = scrape.get_text()
   
     emails = set(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}", scrape))
     email_list = list(emails)
+    for item in emailsManual:
+        email_list.insert(0, item)
        
     if enviar_email:
       for row in zip(email_list):
@@ -27,10 +28,10 @@ def start_scrape(subject:str, content:str, sender:str, page=''):
             msg = EmailMessage()
             msg['Subject'] = subject
             msg.set_content(content)
-            msg['From'] = EMAIL_ADRESS
+            msg['From'] = login
             msg['To'] = row[0]
             print(row[0])
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-              smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
+              smtp.login(login, password)
               smtp.auth_login()
               smtp.send_message(msg)
